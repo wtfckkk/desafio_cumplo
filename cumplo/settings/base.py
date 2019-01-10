5b31a320714856cdc154257b729fedec37aca2fb
Tomás C <tcornejo01@gmail.com>
2019-01-10 03:24:57 -0300

    Update base.py

--- cumplo/settings/base.py
+++ cumplo/settings/base.py
@@ -125,7 +125,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 STATIC_URL = '/static/'
-STATIC_ROOT = '/home/ubuntu/desafio_cumplo/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static/'), '/static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = '/home/ubuntu/desafio_cumplo/static'
 
 API_KEY_SBIF = 'a6d97a548aae4e36c8fcf0afc814daa47e44506d'
